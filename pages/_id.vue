09d421d94e4aa0714d4f559a0d71173c644833f1
backsoul <danielsarmientoenterprise@gmail.com>
2021-11-06 18:57:42 -0500

    width image

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,21 +1,41 @@
 <template>
   <div v-if="article" class="mx-4 flex justify-center mb-20">
     <div class="flex justify-center flex-col w-full 2xl:w-2/3">
-    <img :src="`https://res.cloudinary.com/backsoul/image/upload/v1/${article.image}`" alt="" srcset="" style="object-fit: cover;" class="h-60 2xl:h-96">
-    <p class="mt-5" style="color: #c3bdbd;font-weight: 400;font-family: inherit;">{{convertDate(article.createdAt)}}</p>
-    <h1 class="font-medium text-center my-5 text-3xl 2xl:text-4xl text-primary" style="text-align: start;font-family: fangsong;">{{ article.title }}</h1>
-    <div class="flex">
-    <Category v-for="category of article.categories" :key="category.id" :category="category" />
-    </div>
-    <div v-html="markdownToHtml" class="content-html"></div>
+      <img
+        :src="`https://res.cloudinary.com/backsoul/image/upload/v1/${article.image}`"
+        alt=""
+        srcset=""
+        style="object-fit: cover"
+        class="h-60 2xl:h-96"
+      />
+      <p
+        class="mt-5"
+        style="color: #c3bdbd; font-weight: 400; font-family: inherit"
+      >
+        {{ convertDate(article.createdAt) }}
+      </p>
+      <h1
+        class="font-medium text-center my-5 text-3xl 2xl:text-4xl text-primary"
+        style="text-align: start; font-family: fangsong"
+      >
+        {{ article.title }}
+      </h1>
+      <div class="flex">
+        <Category
+          v-for="category of article.categories"
+          :key="category.id"
+          :category="category"
+        />
+      </div>
+      <div v-html="markdownToHtml" class="content-html"></div>
     </div>
   </div>
 </template>
 
 <script>
-import marked from 'marked';
-import { ARTICLE_BY_SLUG } from '~/graphql/query';
-import dayjs from 'dayjs';
+import marked from 'marked'
+import { ARTICLE_BY_SLUG } from '~/graphql/query'
+import dayjs from 'dayjs'
 export default {
   data() {
     return {
@@ -23,74 +43,75 @@ export default {
     }
   },
   computed: {
-   markdownToHtml(){
-     return marked(this.article.content);
-   }
- },
-  methods:{
-    convertDate(date){
-      return dayjs(date).format('DD MMMM YYYY HH:mm:ss');
+    markdownToHtml() {
+      return marked(this.article.content)
     },
-    getArticle(slug){
-      this.$apollo.query({
-        query:ARTICLE_BY_SLUG,
-        variables:{
-          slug
-        }
-      }).then(({data})=>{
-          this.$store.dispatch('loading', false)
-        this.article = data.articleBySlug;
-      })
-    }
   },
-  mounted(){
+  methods: {
+    convertDate(date) {
+      return dayjs(date).format('DD MMMM YYYY HH:mm:ss')
+    },
+    getArticle(slug) {
+      this.$apollo
+        .query({
+          query: ARTICLE_BY_SLUG,
+          variables: {
+            slug,
+          },
+        })
+        .then(({ data }) => {
+          this.$store.dispatch('loading', false)
+          this.article = data.articleBySlug
+        })
+    },
+  },
+  mounted() {
     this.getArticle(this.$route.params.id)
-      this.$store.dispatch('loading', true)
-  }
+    this.$store.dispatch('loading', true)
+  },
 }
 </script>
 <style lang="scss">
-.content-html{
-      font-size: 1.2rem;
-    font-family: inherit;
+.content-html {
+  font-size: 1.2rem;
+  font-family: inherit;
+  font-weight: 400;
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  color: var(--text-color-primary);
+  > h2 {
+    font-size: 1.5rem;
     font-weight: 400;
-    margin-top: 1rem;
-    display: flex;
-    flex-direction: column;
+    font-family: inherit;
+  }
+  > p {
+    font-size: 1.2rem;
+    font-weight: 400;
+    font-family: inherit;
+    text-align: start;
+  }
+  > blockquote {
     width: 100%;
-    color: var(--text-color-primary);
-    > h2{
-      font-size: 1.5rem;
-      font-weight: 400;
-      font-family: inherit;
-
-    }
-    > p{
-      font-size: 1.2rem;
-      font-weight: 400;
-      font-family: inherit;
-      text-align: start;
-    }
-    > blockquote{
-      width: 100%;
-      height: auto;
-       margin: 1rem 0;
-      > p {
-        > img{
-      width: 100%;
-                    height: auto;
-                    object-fit: cover;
-                     height: 10rem;
-        }
-        @media screen and (min-width: 1040px){
-              > img {
-              height: 35rem;
-            }
+    height: auto;
+    margin: 1rem 0;
+    > p {
+      > img {
+        width: 100vw;
+        height: auto;
+        object-fit: cover;
+        height: 10rem;
+      }
+      @media screen and (min-width: 1040px) {
+        > img {
+          height: 35rem;
         }
       }
     }
+  }
 }
-html{
+html {
   background-color: white;
 }
 </style>
